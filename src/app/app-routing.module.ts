9d628f3f023005b863ebad94b9d66237f04c77a2
Supun Praneeth <spmsupun@gmail.com>
2018-06-02 01:31:52 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {SectionComponent} from './section/section.component';
-import {ResumeComponent} from './resume/resume.component';
 
 
-const routes: Routes = [
-  {path: '', component: SectionComponent},
-  {path: 'cv', component: ResumeComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
